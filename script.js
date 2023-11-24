c288164eb5faaf6f4f6e3cb4e67472cd6c83e64d
unknown <duncanadoro@gmail.com>
2023-11-24 13:59:14 +0300

    commit message goes here

---
+++ script.js
@@ -0,0 +1,6 @@
+function opentab(tabname)
+let tablinks =document.getElementsByClassName("tab-links");
+let tabcontents =document.getElementsByClassName("tab-contents");
+
+(tablink of tablinks){
+    tablink.classlist.remove("active-link");
